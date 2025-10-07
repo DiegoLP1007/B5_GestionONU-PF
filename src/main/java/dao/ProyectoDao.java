eb815043b8045f9b7902275ca0fc78e73f069187
Informatica <informatica@alumno.local>
2025-10-07 09:19:18 -0600

    T17 - Creación de Dao para Proyectos

---
+++ src/main/java/dao/ProyectoDao.java
@@ -0,0 +1,67 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import model.Proyecto;
+
+public class ProyectoDao {
+    private EntityManagerFactory enti = Persistence.createEntityManagerFactory("gestionOnuPU");
+
+    public void guardar(Proyecto proyecto) {
+        EntityManager admin = enti.createEntityManager();
+        EntityTransaction transaccion = admin.getTransaction();
+        
+        try {
+            transaccion.begin();
+            admin.persist(proyecto);
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive())  transaccion.rollback();
+        } finally {
+            admin.close();
+        }
+    }
+    
+    public Proyecto buscarPorID(int id) {
+        EntityManager admin = enti.createEntityManager();
+        try {
+            return admin.find(Proyecto.class, id);
+        } finally {
+            admin.close();
+        }
+    }
+    
+    public void Actualizar (Proyecto proyecto) {
+        EntityManager admin =  enti.createEntityManager();
+        EntityTransaction transaccion = admin.getTransaction();
+        
+        try {
+            transaccion.begin();
+            admin.merge(proyecto);
+            transaccion.commit();
+        } catch (Exception e) {
+            if(transaccion.isActive()) transaccion.rollback();
+        } finally {
+            admin.close();
+        }
+    }
+    
+    public void Eliminar (int id) {
+        EntityManager admin = enti.createEntityManager();
+        EntityTransaction transaccion = admin.getTransaction();
+        try {
+            transaccion.begin();
+            Proyecto proyecto = admin.find(Proyecto.class, id);
+            if (proyecto != null) {
+                admin.remove(proyecto);                
+            }
+            transaccion.commit();
+        } catch (Exception e) {
+            if(transaccion.isActive()) transaccion.rollback();
+        } finally {
+            admin.close();
+        }
+    }
+}
