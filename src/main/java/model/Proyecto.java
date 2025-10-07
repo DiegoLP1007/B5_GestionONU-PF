a0561ed32f300b5e0d92942b1cc3fa47338baa8c
Informatica <informatica@alumno.local>
2025-10-07 09:17:55 -0600

    T12 - Creación de Modelo de entidad Proyectos

---
+++ src/main/java/model/Proyecto.java
@@ -0,0 +1,88 @@
+package model;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name ="Proyectos")
+public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idProyecto;
+    @Column (name ="nombre", nullable = false)
+    private String nombre;
+    @Column (name ="descripcion", nullable = false)
+    private String descripcion;
+    @Column (name = "fechaInicio")
+    private LocalDate fechaInicio;
+    @Column (name = "fechaFin")
+    private LocalDate fechaFin;
+    @Column (name ="estado", nullable = false)
+    private String estado;
+
+    public Proyecto() {
+    }
+
+    public Proyecto(String nombre, String descripcion, LocalDate fechaInicio, LocalDate fechaFin, String estado) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+    }
+
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
